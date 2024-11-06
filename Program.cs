2f8adf89adfe7c5578d8462763fa9e19e651e3cd
Sérgio Pavanelli <sergiopavanelli@gmail.com>
2024-11-06 10:18:06 -0300

    Program.cs final update

--- Program.cs
+++ Program.cs
@@ -38,7 +38,7 @@ builder.Services.AddAuthentication(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
